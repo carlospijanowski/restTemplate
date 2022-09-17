b8729e30bcea59393764ff244068d80b68a0851b
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2022-09-17 20:46:22 -0300

    Initial commit

---
+++ src/test/java/br/com/seteideias/resttemplate/RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
